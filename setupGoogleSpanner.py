366e6966aecfff68d8c5fc4c0f416d4f6bda4681
JustinR7840 <ramosjus@sonoma.edu>
2017-12-05 17:34:44 -0800

    Python setup script to create GoogleSpanner DB
    setupGoogleSpanner.py will create the GoogleSpanener instance and/or
    datbase if they do not already exist.
    google-cloud-spanner is required. Run "pip3 install -r requirements.txt"
    to install the module. pip3 is required because this is a python3 script.
    So run it with pyrhon3 too. May also work with python2.7 though, I'm
    haven't tested.
    The GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS environment
    variables need to be set before running the script. Read the comments at
    the top of the script for more information on what these should be.
    The config options for the script are currently hardcoded. This is
    probably fine, they'll just need to be modified by the user.
    Feel free to modify the script however you want.

---
+++ setupGoogleSpanner.py
@@ -0,0 +1,64 @@
+from google.cloud import spanner
+
+# The GCLOUD_PROJECT environment variable should be specified with a value
+# equal to the name of the Google Cloud project id.
+# The GOOGLE_APPLICATION_CREDENTIALS environment variable should be specified
+# with a value equal to the location of the api key.
+
+# This script will try to create the GoogleSpanner instance and database if
+# they do not already exist.
+
+instance_name = "<SPANNER_INSTANCE_NAME_HERE>"
+db_name = "<DATABASE_NAME_HERE>"
+region = "<INSTANCE_LOCATION_HERE>" # Available regions can be found at
+									# https://cloud.google.com/spanner/docs/instances#available-configurations-regional
+									# eg us-central1
+
+
+mInstance = None
+foundInstance = False
+mDatabase = None
+foundDatabase = False
+
+spanner_client = spanner.Client()
+
+# Get all instances for project and try to find our own instance
+for instance in spanner_client.list_instances():
+	if(instance_name in instance.instance_id):
+		foundInstance = True
+		mInstance = instance
+		break
+
+# Create an instance if one does not exist
+if(not foundInstance):
+	mConfig = None
+
+	for config in spanner_client.list_instance_configs():
+		# We want the config for the region we will be deploying in
+		if(region in config.display_name):
+			mConfig = config
+			break
+
+	instance = spanner_client.instance(instance_name,
+	                                   configuration_name=mConfig.name,
+	                                   node_count=1,
+	                                   display_name="My instance")
+	operation = instance.create()
+	# This will wait until the instance is created before moving on
+	operation.result()
+	instance.reload()
+	mInstance = instance
+
+# Look through the databases in the instance to try and find ours
+for database in mInstance.list_databases():
+	if(db_name in database.database_id):
+		foundDatabase = True
+		mDatabase = database
+		break
+
+# Create the database if it does not exist
+if(not foundDatabase):
+	mDatabase = mInstance.database(db_name)
+	operation = mDatabase.create()
+	# Same thing, wait until the db is created before moving on
+	operation.result()
